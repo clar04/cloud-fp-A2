2b3303a3b6fc4613f32754742a89100e04a3a0f4
clar04 <123356941+clar04@users.noreply.github.com>
2023-12-16 22:29:41 +0700

    Delete azure directory

--- azure/AzureApp.py
+++
@@ -1,5 +0,0 @@
-import azure.AzureApp as func
-from ..flask import app
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
